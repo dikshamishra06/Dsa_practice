8246efe1f45dcbfc7016d51aaa50a6758acb726b
Diksha Mishra <mishradiksha302@gmail.com>
2026-01-04 10:41:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            int count = 0;
+            int s = 0;
+            for(int j=1;j<=sqrt(nums[i]);j++){
+                if(nums[i]%j==0){
+                    count++;
+                    s += j;
+                    if(j != nums[i]/j) {
+                        s += nums[i]/j;
+                        count++;
+                    }
+                }
+                if(count==4) break;
+            }
+            if(count==4) sum += s;
+        }
+        return sum;
+    }
+};
