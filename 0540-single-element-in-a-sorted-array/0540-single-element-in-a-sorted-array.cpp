e696071b044e8d720a23f1cc7e235563f93d6a79
Diksha Mishra <mishradiksha302@gmail.com>
2026-01-25 12:32:06 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.54%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,20 +1,20 @@
+// 2nd brute force //
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return nums[0];
-        int a = nums[0];
-        int count = 1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==a) count++;
-            else{
-                if(count<2) return nums[i-1];
-                else {
-                    a = nums[i];
-                    count = 1;
-                }
+        for(int i=0;i<n;i++){
+            if(i==0){
+                if(nums[i]!=nums[i+1]) return nums[i];
+            }
+            else if(i==n-1){
+                if(nums[i]!=nums[i-1]) return nums[i];
+            }
+            else {
+                if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
             }
         }
-        return a;
+        return -1;
     }
 };
