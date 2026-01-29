a7dc86f9b2343bd386df4bc9314ced53ec5f5ce7
Diksha Mishra <mishradiksha302@gmail.com>
2026-01-30 01:25:14 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,23 +4,19 @@ public:
         int n = s.size();
         stack<char>st;
         for(int i=0;i<n;i++){
+            //opening //
             if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
-            else if(s[i]==')' || s[i]==']' || s[i]=='}'){
-                if(s[i]==')'){
-                    if(st.top()=='(') st.pop();
-                    else return false;
+            // closing //
+            else{
+                if(st.size()==0){
+                    return false;
                 }
-                if(s[i]==']'){
-                    if(st.top()=='[') st.pop();
-                    else return false;
+                else if((st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']') || (st.top()=='{' && s[i]=='}')){
+                    st.pop();
                 }
-                if(s[i]=='}'){
-                    if(st.top()=='{') st.pop();
-                    else return false;
-                }
+                else return false;
             }
         }
-        if(st.empty()) return true;
-        else return false;;
+        return st.size()==0;
     }
 };
