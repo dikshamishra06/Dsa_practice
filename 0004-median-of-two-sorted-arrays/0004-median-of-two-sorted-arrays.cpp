2831fca6e3af1bafdd1c03db0e7fa5bc3ecc247a
Diksha Mishra <mishradiksha302@gmail.com>
2026-02-18 09:32:55 +0530

    Time: 1 ms (38.33%), Space: 95.2 MB (62.72%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size() + nums2.size();
-        int i=0, j=0, k=0;
-        vector<int> ans(n);
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]) {
-                ans[k++] = nums1[i++];
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int n = n1+n2;
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int indx2 = n/2;
+        int indx1 = indx2 - 1;
+        int indxele1 = -1;
+        int indxele2 = -1;
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]){
+                if(count==indx1) indxele1 = nums1[i];
+                if(count==indx2) indxele2 = nums1[i];
+                i++;
+                count++;
             }
-            else ans[k++] = nums2[j++];
+            else {
+                if(count==indx1) indxele1 = nums2[j];
+                if(count==indx2) indxele2 = nums2[j];
+                j++;
+                count++;
+            }    
         }
-        while(i<nums1.size()){
-            ans[k++] = nums1[i++];
+        while(i<n1){
+            if(count==indx1) indxele1 = nums1[i];
+                if(count==indx2) indxele2 = nums1[i];
+                i++;
+                count++;
         }
-        while(j<nums2.size()){
-            ans[k++] = nums2[j++];
+        while(j<n2){
+            if(count==indx1) indxele1 = nums2[j];
+            if(count==indx2) indxele2 = nums2[j];
+            j++;
+            count++;
         }
-        if(n%2==0) return (double)(ans[n/2] + ans[n/2-1])/2;
-        else return (double)ans[n/2];
+        if(n%2==0) return double((double)(indxele1 + indxele2)/2.0);
+        else return double(indxele2);
     }
 };
