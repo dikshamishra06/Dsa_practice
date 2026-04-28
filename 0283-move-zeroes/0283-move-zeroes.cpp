d491913e564e9a9c8bf59f825157c4bad550fbc5
Diksha Mishra <mishradiksha302@gmail.com>
2026-04-28 16:47:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        if(n==0 || n==1) return;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                for(int j=i;j<n;j++){
-                    if(nums[j]!=0){
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        break;
-                    }
-                }
+        int i = 0;
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] =  temp;
+                i++;
             }
         }
-        return ;
     }
 };
