03c8b41a1773a08bda2e312f1d503827176036fe
Diksha Mishra <mishradiksha302@gmail.com>
2026-05-16 11:51:40 +0530

    Time: 0 ms (100%), Space: 40.3 MB (58.4%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, 1);
-
-        // prefix products
-        for (int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] * nums[i - 1];
+        vector<int>ans(n);
+        // left product //
+        ans[0] = 1;
+        for(int i=1;i<n;i++){
+            ans[i] = nums[i-1] * ans[i-1];
         }
-
-        // suffix products
-        int suffix = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            suffix *= nums[i + 1];
-            ans[i] *= suffix;
+        // an right product nikalengye aur uska multiplication karengye left    product se //
+        int right = 1;
+        for(int i=n-1;i>=0;i--){
+            ans[i] = right * ans[i];
+            right *= nums[i];
         }
-
         return ans;
     }
 };
