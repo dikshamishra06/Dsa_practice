61ea5c74b1ab6e10abf5db77c32c376ff7e63228
Diksha Mishra <mishradiksha302@gmail.com>
2026-05-23 10:44:32 +0530

    Time: 7 ms (48.19%), Space: 7.7 MB (65.1%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+int ans(int n){
+    if(n ==0 || n==1) return n;
+    int fibo = ans(n-2) + ans(n-1);
+    return fibo;
+}
     int fib(int n) {
-     if(n==0||n==1) return n;
-     return (fib(n-1) + fib(n-2));
+        return ans(n);
     }
 };
